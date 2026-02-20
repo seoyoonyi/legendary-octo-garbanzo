6c2e65681a464a676eeb5d5623baaec7a26d14a7
seoyoon yi <89791868+seoyoonyi@users.noreply.github.com>
2026-02-20 23:50:31 +0900

    Title: 친근하지 않은 수, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260220/친근하지 않은 수/unfriendly-number.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const N = Number(fs.readFileSync(0).toString().trim());
+
+let count = 0;
+
+for (let i = 1; i <= N; i++) {
+    if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0) {
+        count++;
+    }
+}
+
+console.log(count);
