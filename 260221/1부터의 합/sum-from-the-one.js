28f0a23db7c40825fc2edcb5c167f12e38360bd4
seoyoon yi <89791868+seoyoonyi@users.noreply.github.com>
2026-02-21 17:30:31 +0900

    Title: 1부터의 합, Time: 70ms, Memory: 10MB, Status: Passed - Codetree

--- 260221/1부터의 합/sum-from-the-one.js
+++ 260221/1부터의 합/sum-from-the-one.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const N = Number(fs.readFileSync(0).toString().trim())
 let result = 0
 
-for(let i = 1; i<100; i++){
+for(let i = 1; i<=100; i++){
     result += i
     if(result >= N){
         console.log(i)
