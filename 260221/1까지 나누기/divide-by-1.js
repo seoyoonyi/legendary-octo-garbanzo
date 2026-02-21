5778e8f326f1a6a8e4e6d8c179e21f05f30fc2d9
seoyoon yi <89791868+seoyoonyi@users.noreply.github.com>
2026-02-21 18:34:25 +0900

    Title: 1까지 나누기, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260221/1까지 나누기/divide-by-1.js
+++ 260221/1까지 나누기/divide-by-1.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
 const N = Number(fs.readFileSync(0).toString().trim());
-let count = 1;
-let result = N
+let count = 0;
+let result = N;
 
-for(let i = 1; i<=5000; i++){
-    count += 1
+for(let i = 1; ; i++){
     result = Math.floor(result/i)
-    if(Math.floor(result/i) < 1) break;
+    count += 1
+
+    if(result <= 1) break;
 }
 
 console.log(count)
