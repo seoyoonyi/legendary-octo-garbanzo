0141c5a999469d6c0d4d15cd2645b7ec7db41729
seoyoon yi <89791868+seoyoonyi@users.noreply.github.com>
2026-02-23 23:46:41 +0900

    Title: 2의 거듭제곱수, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

--- 260223/2의 거듭제곱수/pow-of-2.js
+++ 260223/2의 거듭제곱수/pow-of-2.js
@@ -1,5 +1,12 @@
-const fs = require('fs')
-const input = Number(fs.readFileSync(0).toString().trim());
-const result = Math.floor(input/2)
+const fs = require('fs');
+const N = Number(fs.readFileSync(0).toString().trim());
 
-console.log(result)
+let x = 0;
+let value = N;
+
+while (value > 1) {
+    value /= 2;
+    x++;
+}
+
+console.log(x);
